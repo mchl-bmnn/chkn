61fbf871dc74b83e8e629939e62c23fc91bb1435
q372397 <michael.bd.baumann@bmw.de>
2018-02-17 20:52:19 +0100

    Bugfix - missed reference to handler in project when renaming

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [ring/ring-json "0.4.0"]
                  ]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler chkn.handler/app :port 8080}
+  :ring {:handler chkn.handler/handler :port 8080}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
